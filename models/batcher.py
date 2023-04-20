6faf43a8c43f726a5314c4c37381f9e61672dcd2
Kieron Kretschmar <k.kretschmar@getraenkeland-online.com>
2023-04-20 19:13:02 +0200

    Adapt Batcher to SentEval interface

--- models/batcher.py
+++ models/batcher.py
@@ -15,13 +15,22 @@ class Batcher():
             batch (list of str): List of sentences.
         """
        
+        # Transform each sentence into a single string
+        # Empty sentences will be replaced by the unknown token
+        seqs = [" ".join([tokens for tokens in seq]) if len(seq) > 0 else self.vocab.unk_token for seq in batch]
+
         # Transform each sentence from string to a variable length tensor of word embeddings
-        seqs = [torch.stack([self.vocab.get_embedding(token) for token in nltk.word_tokenize(seq)], dim=0) for seq in batch]
+        seqs = [torch.stack([self.vocab.get_embedding(token) for token in nltk.word_tokenize(seq)], dim=0) for seq in seqs]
+
+        # Store sequence lengths for encoder
+        seq_lengths = [len(seq) for seq in seqs]
 
         # Transform into a single padded tensor 
         seqs = torch.nn.utils.rnn.pad_sequence(seqs, batch_first=True)
         
         # Transform batch tensor into a single sentence embedding
-        seq_embs = self.model(seqs)   # (B, SeqEmb)
+        seq_embs = self.model(seqs, seq_lengths)   # (B, SeqEmb)
+
+        seq_embs = seq_embs.detach().numpy()
 
         return seq_embs
