510e05bbb3333aeac0326fe829c8f90176812761
Kieron Kretschmar <k.kretschmar@getraenkeland-online.com>
2023-04-21 10:41:31 +0200

    Remove unused code

--- data/snli_dataset.py
+++ data/snli_dataset.py
@@ -19,33 +19,6 @@ class SNLIDataset(Dataset):
             example["label"])
             for example in tqdm.tqdm(unprocessed_data) if example["label"] > 0]      # Remove samples with broken labels < 0
 
-        # self.data = []
-        # for index in tqdm.tqdm(range(len(unprocessed_data["premise"]))):
-        #     premise = word_embeddor.embed(unprocessed_data["premise"][index])
-        #     hypothesis = word_embeddor.embed(unprocessed_data["hypothesis"][index])
-
-            
-        #     premise = word_embeddor.embed(unprocessed_data["premise"][index])
-        #     hypothesis = word_embeddor.embed(unprocessed_data["hypothesis"][index])
-            # label = unprocessed_data["label"][index]
-            # if label < 0:
-            #     print(f"Invalid label found at index {index}. Dismissing sample.")
-            #     continue
-            # self.data.append((premise, hypothesis, label))
-
-        # self.data = []
-        # for i in tqdm.tqdm(range(0, len(unprocessed_data["premise"]), self.chunk_size)):
-        #     premise = word_embeddor.embed(unprocessed_data["premise"][i:i + self.chunk_size])
-        #     hypothesis = word_embeddor.embed(unprocessed_data["hypothesis"][i:i + self.chunk_size])
-        #     label = torch.tensor(unprocessed_data["label"][i:i + self.chunk_size]).to(word_embeddor.device)
-
-        #     valid_mask = label > 0
-        #     if not valid_mask.all():
-        #         print(f"Invalid label found at chunk index {i}. Dismissing sample.")
-        #         continue
-
-        #     self.data.append((premise, hypothesis, label))
-
     def __getitem__(self, index):
         return self.data[index]
 
